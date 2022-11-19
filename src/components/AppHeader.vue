8d5494772d69b4762524520ba1b60674f1e80a04
Gabriel Heinrich <gabriel@codecampleipzig.de>
2022-11-19 11:35:08 +0100

    Work on Editor

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -34,13 +34,13 @@
 </template>
 
 <script setup lang="ts">
-import { useRouter } from "vue-router"
+import { useRouter } from "vue-router";
 
-const router = useRouter()
+const router = useRouter();
 
 const createNewRoutine = () => {
-  router.push({ name: "RoutineEditor", params: { id: "new" } })
-}
+  router.push({ name: "RoutineEditor", params: { id: "new" } });
+};
 </script>
 
 <style scoped></style>
--- src/views/RoutineEditor.vue
+++ src/views/RoutineEditor.vue
@@ -2,15 +2,88 @@
   <div class="grid grid-rows-[auto_1fr] h-screen w-screen">
     <AppHeader class="border-b" />
     <div class="grid grid-cols-[auto_1fr_auto] h-full">
-      <div class="border-r max-w-sm w-full h-full">Assets</div>
-      <div>Preview</div>
-      <div class="border-r max-w-sm w-full h-full">Inspector</div>
+      <div class="border-r max-w-sm w-screen h-full grid grid-rows-[auto_1fr]">
+        <header class="p-2">
+          <h2 class="mb-1 text-sm uppercase font-semibold tracking-wide">
+            Exercise Library
+          </h2>
+
+          <input
+            class="w-full border rounded-md px-2 py-1"
+            type="text"
+            name=""
+            id=""
+            placeholder="Search..."
+          />
+        </header>
+
+        <div class="relative">
+          <div class="absolute inset-0 overflow-auto">
+            <div
+              class="grid gap-2 px-2"
+              :style="{
+                gridTemplateColumns: 'repeat(auto-fit, minmax(100px, 1fr))',
+              }"
+            >
+              <div
+                v-for="clip in 100"
+                :key="clip"
+                class="aspect-video bg-black"
+              >
+                {{ clip }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="px-2 py-1 grid grid-rows-[auto_1fr_auto] gap-2">
+        <div>
+          <h2>Name of routine</h2>
+        </div>
+        <div class="grid place-content-center">
+          <div
+            class="aspect-video bg-black max-w-screen-sm w-screen mx-auto"
+          ></div>
+        </div>
+        <div>
+          <div class="relative h-24">
+            <div class="absolute inset-0 overflow-auto w-full">
+              <div class="flex space-x-2">
+                <div
+                  v-for="placeholder in 100"
+                  :key="placeholder"
+                  class="flex-shrink-0"
+                  @click="selection = 'Section ' + placeholder"
+                >
+                  <div>{{ placeholder }}</div>
+                  <RoutineEditorClip />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div></div>
+        </div>
+      </div>
+      <div class="border-l max-w-sm w-screen h-full p-2">
+        <h2 class="mb-1 text-sm uppercase font-semibold tracking-wide">
+          Inspector
+        </h2>
+        <h3>
+          {{ selection }}
+        </h3>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import AppHeader from "@/components/AppHeader.vue";
+import RoutineEditorClip from "@/components/RoutineEditorClip.vue";
+
+import { ref } from "vue";
+
+const selection = ref<string>();
 </script>
 
 <style scoped></style>
