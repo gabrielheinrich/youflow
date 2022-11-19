43612e53886c38e2aacf18df65d2e6f35a474c73
Hannes Kochniß <hannes@hkochniss.de>
2022-11-19 20:11:45 +0100

    fix player controls

--- src/views/RoutinePlayer.vue
+++ src/views/RoutinePlayer.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="grid place-content-center w-screen h-screen relative">
+  <div
+    class="grid place-content-center w-screen h-screen relative"
+    @click="playOrPause"
+  >
     <div ref="videoElement" class="w-screen h-screen relative">
       <div id="player" class="w-full h-full" :class="iframeClass"></div>
       <div
@@ -15,6 +18,45 @@
         }"
       ></div>
     </div>
+    <div class="absolute w-full h-full flex items-center justify-center">
+      <transition
+        enter-active-class="transition-[transform,opacity] duration-500 ease-out"
+        enter-from-class="scale-75 opacity-100"
+        enter-to-class="scale-105 opacity-80"
+        leave-active-class="hidden"
+      >
+        <div
+          v-if="showSymbol == 'pause'"
+          class="text-white rounded-full bg-gray-800/60 p-4"
+        >
+          <i-material-symbols-pause-outline class="w-16 h-16">
+          </i-material-symbols-pause-outline>
+        </div>
+      </transition>
+      <transition
+        enter-active-class="transition-[transform,opacity] duration-500 ease-out"
+        enter-from-class="scale-75 opacity-100"
+        enter-to-class="scale-105 opacity-70"
+        leave-active-class="hidden"
+      >
+        <div
+          v-if="showSymbol == 'play'"
+          class="text-white rounded-full bg-gray-800/60 p-4"
+        >
+          <i-material-symbols-play-arrow class="w-16 h-16">
+          </i-material-symbols-play-arrow>
+        </div>
+      </transition>
+    </div>
+  </div>
+  <div
+    class="absolute flex gap-4 justify-center items-end top-2 h-[100px] w-screen z-[1000] bg-black"
+  >
+    <div class="p-4 bg-gray-900 rounded-md text-gray-200 text-xl">
+      <span class="">{{ activeExercise?.name }}</span>
+      &nbsp;
+      <span>{{ activeIndexString }}</span>
+    </div>
   </div>
   <div
     class="absolute flex gap-4 justify-center items-center bottom-2 h-[180px] w-screen z-[1000]"
@@ -23,7 +65,7 @@
     <div class="rounded-lg flex justify-center gap-4 w-64 h-8 text-white">
       <button
         @click="goBack"
-        class="bg-gray-900 hover:bg-gray-800 transition-colors cursor-pointer flex items-center justify-center w-16 h-16 rounded"
+        class="bg-gray-900 hover:bg-gray-800 transition-colors cursor-pointer flex items-center justify-center w-16 h-14 rounded"
       >
         <i-material-symbols-arrow-back-ios-rounded
           class="w-5 h-5 translate-x-1"
@@ -31,7 +73,7 @@
       </button>
       <button
         @click="playOrPause"
-        class="bg-gray-900 hover:bg-gray-800 transition-colors cursor-pointer flex items-center justify-center w-16 h-16 rounded"
+        class="bg-gray-900 hover:bg-gray-800 transition-colors cursor-pointer flex items-center justify-center w-16 h-14 rounded"
       >
         <i-material-symbols-play-arrow
           v-if="playerState != 1"
@@ -44,7 +86,7 @@
       </button>
       <button
         @click="goNext"
-        class="bg-gray-900 hover:bg-gray-800 transition-colors cursor-pointer flex items-center justify-center w-16 h-16 rounded"
+        class="bg-gray-900 hover:bg-gray-800 transition-colors cursor-pointer flex items-center justify-center w-16 h-14 rounded"
       >
         <i-material-symbols-arrow-forward-ios-rounded
           class="w-5 h-5 translate-x-0"
@@ -53,7 +95,7 @@
 
       <button
         @click="exitVideo"
-        class="bg-gray-900 hover:bg-gray-800 transition-colors cursor-pointer flex items-center justify-center w-16 h-16 rounded"
+        class="bg-gray-900 hover:bg-gray-800 transition-colors cursor-pointer flex items-center justify-center w-16 h-14 rounded"
       >
         <i-material-symbols-fullscreen-exit
           class="w-5 h-5 translate-x-0"
@@ -64,11 +106,10 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, watch } from "vue"
+import { ref, onMounted, watch, computed } from "vue"
 import YoutubeIFrameLoader from "youtube-iframe"
 import { useRouter } from "vue-router"
 import { getWorkoutStore } from "@/stores/workoutStore"
-import { Workout } from "@/types"
 
 const workoutStore = getWorkoutStore()
 
@@ -78,13 +119,32 @@ const props = defineProps<{
   id: string
 }>()
 
+type SymbolType = "none" | "play" | "pause"
 const iframeClass = ref("blackedout")
+const showSymbol = ref("none" as SymbolType)
 
 const setShowVideo = (shouldShow: boolean) => {
   iframeClass.value = shouldShow ? "visible" : "blackedout"
 }
 
 const workout = workoutStore.workouts.find((w) => w.id == props.id)!
+const stepIndex = ref(0)
+/** used to cancel current video play on skip */
+let controller = new AbortController()
+
+const activeTimelineStep = computed(() => workout.timeline[stepIndex.value])
+const activeExercise = computed(
+  () =>
+    workoutStore.exercises.find(
+      (e) => e.id == activeTimelineStep.value.exerciseId
+    )!
+)
+const activeIndexString = computed(
+  () => stepIndex.value + 1 + " / " + workout.timeline.length
+)
+
+const waitMs = async (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms))
 
 setShowVideo(false)
 
@@ -100,19 +160,43 @@ setShowVideo(false)
 const playerState = ref(-1 as YT.PlayerState)
 const paused = ref(false)
 let ytPlayer: YT.Player
+let symbolTimer: any
 
-const goNext = () => router.back()
-const goBack = () => router.back()
+const goNext = () => {
+  if (stepIndex.value == workout.timeline.length - 1) return
+  stepIndex.value++
+}
+const goBack = () => {
+  // if we are > 2 seonds into video, get to start of video
+  if (ytPlayer.getCurrentTime() > activeExercise.value.startSecond + 2) {
+    ytPlayer.seekTo(activeExercise.value.startSecond, true)
+    return
+  }
+
+  if (stepIndex.value == 0) return
+
+  stepIndex.value--
+}
+
 const exitVideo = () => router.back()
+
 const playOrPause = () => {
   if (playerState.value == YT.PlayerState.PLAYING) {
     ytPlayer.pauseVideo()
     paused.value = true
+
+    showSymbol.value = "pause"
+    clearTimeout(symbolTimer)
+    symbolTimer = setTimeout(() => (showSymbol.value = "none"), 500)
   } else {
     ytPlayer.playVideo()
     setTimeout(() => {
       paused.value = false
     }, 100)
+
+    showSymbol.value = "play"
+    clearTimeout(symbolTimer)
+    symbolTimer = setTimeout(() => (showSymbol.value = "none"), 500)
   }
 }
 
@@ -145,52 +229,58 @@ const initPlayer = async () => {
   })
 }
 
-const playClip = async (
-  videoId: string,
-  startTime: number,
-  endTime: number
-) => {
-  ytPlayer.loadVideoById(videoId, startTime)
-  // ytPlayer.seekTo(startTime, true)
+const playTimelineStep = async (step: number, signal: AbortSignal) => {
+  const exerciseId = workout.timeline[step].exerciseId
+  const exercise = workoutStore.exercises.find((e) => e.id == exerciseId)!
+
+  ytPlayer.loadVideoById(exercise.srcId, exercise.startSecond)
   ytPlayer.mute()
   ytPlayer.playVideo()
 
-  setTimeout(() => {
-    setShowVideo(true)
-  }, 200)
+  await waitMs(200)
+  setShowVideo(true)
 
   return new Promise((resolve, reject) => {
     const interval = setInterval(() => {
-      console.log(ytPlayer.getCurrentTime())
+      console.log("step " + step, ytPlayer.getCurrentTime())
 
-      if (ytPlayer.getCurrentTime() > endTime - 0.5) {
+      if (ytPlayer.getCurrentTime() > exercise.endSecond - 0.5) {
         setShowVideo(false)
       }
-      if (ytPlayer.getCurrentTime() > endTime) {
+      if (ytPlayer.getCurrentTime() > exercise.endSecond) {
         ytPlayer.stopVideo()
-        // ytPlayer.pauseVideo()
         clearInterval(interval)
-        // ytPlayer.destroy()
+
+        stepIndex.value = stepIndex.value + 1
         resolve(undefined)
       }
     }, 1000)
+
+    signal.addEventListener("abort", () => {
+      ytPlayer.stopVideo()
+      clearInterval(interval)
+      reject(new DOMException("Video Play Cancelled", "AbortError"))
+    })
   })
 }
 
-const playWorkout = async (wo: Workout) => {
-  await initPlayer()
-
-  for (const timelineItem of wo.timeline) {
-    const exercise = workoutStore.exercises.find(
-      (w) => w.id == timelineItem.exerciseId
-    )!
-
-    await playClip(exercise.srcId, exercise.startSecond, exercise.endSecond)
+// play queue: timeline items
+// play next thing which is active thing
+// once play ends "jumpToNext" video and add index
+// watcher on index then controls video player
+watch(
+  () => stepIndex.value,
+  (step) => {
+    // cancel any old play promise in case we skipped segment
+    controller.abort()
+    controller = new AbortController()
+    playTimelineStep(step, controller.signal)
   }
-}
+)
 
 onMounted(async () => {
-  playWorkout(workout)
+  await initPlayer()
+  await playTimelineStep(0, controller.signal)
 })
 </script>
 
