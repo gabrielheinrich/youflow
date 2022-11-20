ab93c2baaee589d6a5b7bb6942982049abe4711a
Gabriel Heinrich <gabriel@codecampleipzig.de>
2022-11-20 13:47:12 +0100

    Update seed data

--- src/seed.ts
+++ src/seed.ts
@@ -2,51 +2,6 @@ import { Exercise, Workout } from "./types";
 
 export const exercises: Exercise[] = [
   {
-    id: "0",
-    name: "Short Test",
-    description: "Simple Test",
-    srcType: "youtube",
-    srcId: "raUEsDttCL4",
-    startSecond: 54,
-    endSecond: 58,
-  },
-  {
-    id: "1",
-    name: "Yoga Stuff",
-    description: "Simple YOLO Yoga",
-    srcType: "youtube",
-    srcId: "raUEsDttCL4",
-    startSecond: 254,
-    endSecond: 280,
-  },
-  {
-    id: "2",
-    name: "Other Stretch",
-    description: "Stretch Awesome",
-    srcType: "youtube",
-    srcId: "ZbtVVYBLCug",
-    startSecond: 535,
-    endSecond: 570,
-  },
-  {
-    id: "3",
-    name: "Cardio",
-    description: "Long Cardio Workout",
-    srcType: "youtube",
-    srcId: "kZDvg92tTMc",
-    startSecond: 466,
-    endSecond: 509,
-  },
-  {
-    id: "4",
-    name: "Meditation",
-    description: "Be calm like Water...",
-    srcType: "youtube",
-    srcId: "ockCQMt9kM0",
-    startSecond: 10,
-    endSecond: 200,
-  },
-  {
     id: "01GJAFZV6P7EQTPP6848NKJP5Z",
     name: "Side Neck Stretch",
     srcType: "youtube",
@@ -57,58 +12,83 @@ export const exercises: Exercise[] = [
     thumbnailUrl:
       "https://kqdtdudmyyxqvimwhuxh.supabase.co/storage/v1/object/public/test/L_xrDAtykMI_61.jpg",
   },
+  {
+    id: "01GJAGCMM1TYVG0Z53QCS92483",
+    name: "Shoulder Stretch",
+    srcType: "youtube",
+    srcId: "L_xrDAtykMI",
+    description: "",
+    startSecond: 126,
+    endSecond: 141,
+    thumbnailUrl:
+      "https://kqdtdudmyyxqvimwhuxh.supabase.co/storage/v1/object/public/test/L_xrDAtykMI_126.jpg",
+  },
+  {
+    id: "01GJAGE5XQWQ6V8FPEKK7V8MB3",
+    name: "Child's Pose Stretch",
+    srcType: "youtube",
+    srcId: "L_xrDAtykMI",
+    description: "",
+    startSecond: 173,
+    endSecond: 188,
+    thumbnailUrl:
+      "https://kqdtdudmyyxqvimwhuxh.supabase.co/storage/v1/object/public/test/L_xrDAtykMI_173.jpg",
+  },
+  {
+    id: "01GJAGFH44Z2WAPSKHRW8P4V2H",
+    name: "Chest Stretch",
+    srcType: "youtube",
+    srcId: "L_xrDAtykMI",
+    description: "",
+    startSecond: 209,
+    endSecond: 224,
+    thumbnailUrl:
+      "https://kqdtdudmyyxqvimwhuxh.supabase.co/storage/v1/object/public/test/L_xrDAtykMI_209.jpg",
+  },
+  {
+    id: "01GJAGGNCT489X6M2S06N3C5T2",
+    name: "Chest Side Stretch",
+    srcType: "youtube",
+    srcId: "L_xrDAtykMI",
+    description: "",
+    startSecond: 256,
+    endSecond: 271,
+    thumbnailUrl:
+      "https://kqdtdudmyyxqvimwhuxh.supabase.co/storage/v1/object/public/test/L_xrDAtykMI_256.jpg",
+  },
+  {
+    id: "01GJAGHVTKCXHAANCZEJPST9RA",
+    name: "Cobra Pose",
+    srcType: "youtube",
+    srcId: "L_xrDAtykMI",
+    description: "",
+    startSecond: 301,
+    endSecond: 316,
+    thumbnailUrl:
+      "https://kqdtdudmyyxqvimwhuxh.supabase.co/storage/v1/object/public/test/L_xrDAtykMI_301.jpg",
+  },
+  {
+    id: "01GJAGKR5AMD375E4H9B28T0M3",
+    name: "Hip Stretch",
+    srcType: "youtube",
+    srcId: "L_xrDAtykMI",
+    description: "",
+    startSecond: 428,
+    endSecond: 443,
+    thumbnailUrl:
+      "https://kqdtdudmyyxqvimwhuxh.supabase.co/storage/v1/object/public/test/L_xrDAtykMI_428.jpg",
+  },
+  {
+    id: "01GJAGNVKQT96ZBRSVECBDEW87",
+    name: "Knee Mobility",
+    srcType: "youtube",
+    srcId: "L_xrDAtykMI",
+    description: "",
+    startSecond: 522,
+    endSecond: 537,
+    thumbnailUrl:
+      "https://kqdtdudmyyxqvimwhuxh.supabase.co/storage/v1/object/public/test/L_xrDAtykMI_522.jpg",
+  },
 ];
 
-export const workouts: Workout[] = [
-  {
-    id: "1",
-    name: "Yoga",
-    createdUtc: 1668946590059,
-    timeline: [
-      {
-        id: "2b8a67a9-35db-4edd-90fd-b30ce51d3544",
-        exerciseId: "0",
-        exerciseReps: 4,
-        cooldownTimeSecs: 30,
-      },
-      {
-        id: "f9a5bf4a-f3fe-48ac-a59b-71772464eef5",
-        exerciseId: "1",
-        exerciseReps: 3,
-        cooldownTimeSecs: 30,
-      },
-      {
-        id: "047306e9-32e0-4017-9300-80b69b0eaabe",
-        exerciseId: "3",
-        exerciseReps: 2,
-        cooldownTimeSecs: 30,
-      },
-    ],
-  },
-  {
-    id: "2",
-    name: "Quick Stretch",
-    createdUtc: 1668946590059,
-    timeline: [
-      {
-        id: "5128a03d-46c1-4c8d-ba41-b9d66c94ce62",
-        exerciseId: "2",
-        exerciseReps: 2,
-        cooldownTimeSecs: 20,
-      },
-    ],
-  },
-  {
-    id: "3",
-    name: "Cardio",
-    createdUtc: 1668946590059,
-    timeline: [
-      {
-        id: "9ba40c18-e58f-419d-9062-7f62cf0046a2",
-        exerciseId: "2",
-        exerciseReps: 2,
-        cooldownTimeSecs: 20,
-      },
-    ],
-  },
-];
+export const workouts: Workout[] = [];
